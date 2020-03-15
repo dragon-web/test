378afea5ff749ed312131dbddf02e72f4ea48746
dragon-web <1582943265@qq.com>
2020-03-15 18:33:27 +0800

    practice

---
+++ day_3_everyday_practice/day_3_everyday_practice/day_2_niuke.c
@@ -0,0 +1,61 @@
+#include"common.h"
+/*
+int FindMaxSum(int *arr,int sz)
+{
+	int sum = 0;
+	int temp = 0;
+	int i = 0, j = 0;
+	if (sz == 1)
+	{
+		return arr[0];
+	}
+	while (arr[i] <= 0 && i < sz)
+	{
+		++i;
+	}//这个i是第一个大于0的数
+	if (i == sz)
+	{
+		return arr[0];
+	}
+	j = i + 1;
+
+
+	
+
+
+}
+
+int main()
+{
+	/*int a;
+	float b, c;
+	float k = 0;
+	scanf("%2d%3f%4f", &a, &b, &c);
+	printf("\na=%d,b=%d,c=%f\n", a, b, c);
+	printf("%f", k);
+	int n[][3] = { 10,20,30,40,50,60 };
+	int(*p)[3];
+	p = n;
+	printf("%d  %d  %d", p[0][0], *(p- 1), (*p)[2]);
+	int x = 1;
+	do
+	{
+		printf("%2d", x++);
+
+	} while (x--);
+
+
+	int arr[] = {1,3,5,-1 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int p = FindMaxSum(arr,sz);
+	printf("%d\n", p);
+	system("pause");
+	return 0;
+}*/
+
+int main()
+{
+
+	system("pause ")
+	return 0;
+}
